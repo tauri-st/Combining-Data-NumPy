9f81bd523fa2337b437991e07e99975cc4c9139d
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-11 00:40:41 +0000

    Open titanic 2, convert to CSV and convert headers and rows then convert to array

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ with open("titanic1.csv", "r") as file:
   titanic_data1 = np.array(data_list)
 
 # ADD CODE: Convert titanic2.csv 
-
+  with open("titanic2.csv", "r") as file:
+    # ADD CODE: Convert titanic2.csv
+    data = csv.reader(file,delimiter=",")
+    headers = next(data)
+    data_list = list(data)
+    titanic_data2 = np.array(data_list)
 
 # ADD CODE: Merge two datasets
 
