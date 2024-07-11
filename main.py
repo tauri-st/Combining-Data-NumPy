a9f524e263fc3c6e869c57f9f795c9d9807624ed
Tauri StClaire <104536950+tauri-st@users.noreply.github.com>
2024-07-11 00:35:05 +0000

    Concert the data headers and rows

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 with open("titanic1.csv", "r") as file:
   # ADD CODE: Convert titanic1.csv
   data1 = csv.reader(file,delimiter=",")
+  headers = next(data1)
+  data_list = list(data1)
 
 # ADD CODE: Convert titanic2.csv 
 
